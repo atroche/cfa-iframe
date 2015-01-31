6196bc3bab2089437f762b70d7dbae6e086a25ae
Alistair Roche <roche.a@gmail.com>
2015-01-31 11:39:25 +1100

    Improve how slimer runs / exits

--- resources/runner.js
+++ resources/runner.js
@@ -28,10 +28,8 @@ fs.write(pagePath, html, 'w');
     	};
 
 			p.onCallback = function (x) {
-    		var line = x.toString();
-    		if (line !== "[NEWLINE]") {
-    			console.log(line.replace(/\[NEWLINE\]/g, "\n"));
-    		}};
+				slimer.exit();
+    		};
 
 
 		p.onError = function(msg) {
@@ -40,24 +38,14 @@ fs.write(pagePath, html, 'w');
 				phantom.exit(1);
 		};
 
+
+
 p.open("file://" + "/Users/aroche/Code/clj/iframe-app/resources/runner.js.html", function () {
 	    fs.remove(pagePath);
 
     // p.evaluate is sandboxed, can't ship closures across;
     // so, a bit of a hack, better than polling :-P
-    var exitCodePrefix = "phantom-exit-code:";
-    p.onAlert = function (msg) {
-	var exit = msg.replace(exitCodePrefix, "");
-	if (msg != exit) phantom.exit(parseInt(exit));
-    };
+
 //
-    p.evaluate(function (exitCodePrefix) {
-	var results = cemerick.cljs.test.run_all_tests();
-	//console.log(results);
-	cemerick.cljs.test.on_testing_complete(results, function () {
-	    window.alert(exitCodePrefix +
-			 (cemerick.cljs.test.successful_QMARK_(results) ? 0 : 1));
-	});
-    }, exitCodePrefix);
 
 });
--- spec/cljs/iframe-app/core_test.cljs
+++ spec/cljs/iframe-app/core_test.cljs
@@ -1,12 +1,21 @@
 (ns core-test
-    (:require-macros [cemerick.cljs.test
-                      :refer (is deftest with-test run-tests testing test-var)])
-    (:require [cemerick.cljs.test :as t]
-              [iframe-app.core]))
+  (:require-macros [cemerick.cljs.test
+                    :refer (is deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test :as t]
+            [iframe-app.core :refer [main]]))
 
 (enable-console-print!)
 
+(set! (.-onload js/window)
+      (fn []
+        (.appendChild (.-body js/document)
+                      (let [element (.createElement js/document "div")
+                            _ (set! (.-id element) "main")]
+                        element))
+        (t/run-all-tests)
+        (.callPhantom js/window "exit")))
+
+
+
 (deftest javascript-allows-div0
          (is (= js/Infinity (/ 1 0) (/ (int 1) (int 0)))))
-
-(println "is this working??! hah lol 2")
